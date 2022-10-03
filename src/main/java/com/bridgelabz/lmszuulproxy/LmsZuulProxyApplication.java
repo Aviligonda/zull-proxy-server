814bf16115e9f50fa6d1a03fcd600105a83d32f9
aviligonda <aviligondasreenivasulu6086@gmail.com>
2022-10-03 19:31:01 +0530

    ADDED : Application.yml file for All Book Store Project Properties and zuul configuration class

--- src/main/java/com/bridgelabz/lmszuulproxy/LmsZuulProxyApplication.java
+++ src/main/java/com/bridgelabz/lmszuulproxy/LmsZuulProxyApplication.java
@@ -7,9 +7,9 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableEurekaClient
 @EnableZuulProxy
 @EnableDiscoveryClient
+//@EnableEurekaClient
 
 public class LmsZuulProxyApplication {
 
